b4b0d8a7e11bb6be9a6493faf2bb3d7bce1d0adb
Federica Pozzer <federica.pozzer@gmail.com>
2023-01-23 16:26:51 +0100

    discount ok

--- js/script.js
+++ js/script.js
@@ -10,15 +10,42 @@
 
 // CALCOLA COSTO BIGLIETTO
 
-// SE UNDER 18 APPLICA SCONTO 40%
+// SE UNDER 18 APPLICA SCONTO 20%
 // SE OVER 65 APPLICA SCONTO 40%
 
 // NON HO CAPITO LA COSA DELLA FORMA UMANA
 
+// ---------------------------------------------------------
 
+// DOMANDE
 let travel_km = parseFloat(prompt("quanti km vuoi percorrere?"));
 let passenger_age = parseInt(prompt("quanti anni hai?"));
 // console.log(travel_km + passenger_age);
 
+// COSTO BIGLIETTO
 let ticket_cost = parseFloat((travel_km * 21) / 100);
 console.log(ticket_cost);
+
+
+// SCONTI
+    // calcolo il 20%
+const discount_20 = (ticket_cost * 20) / 100;
+console.log("sconto 20% " + discount_20);
+    // calcolo il 40%
+const discount_40 = (ticket_cost *40) / 100;
+console.log("sconto 40% " + discount_40);
+ 
+// APPLICO SCONTI
+if (passenger_age <= 18) 
+{
+    let discount_under18 = (ticket_cost - discount_20);
+    console.log("siccome sei minorenne, paghi: " + discount_under18 + " €");
+}
+else if (passenger_age >= 65)
+{
+    let discount_over65 = (ticket_cost - discount_40);
+    console.log("siccome sei over 65, paghi: " + discount_over65 + " €");
+}
+else {
+    console.log("non hai diritto a nessuno sconto, paghi: " + ticket_cost + " €");
+}
