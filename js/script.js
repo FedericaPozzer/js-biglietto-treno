44ddabdf75acd6d011208a80765afd934657a9ce
Federica Pozzer <federica.pozzer@gmail.com>
2023-01-24 11:19:38 +0100

    commit post-correzione!

--- js/script.js
+++ js/script.js
@@ -26,6 +26,7 @@ let passenger_age = parseInt(prompt("quanti anni hai?"));
 
 // COSTO BIGLIETTO
 let ticket_cost = parseFloat((travel_km * 21) / 100);
+// non ho messo 0,21 perchè non sapevo che avrei dovuto scrivere 0.21 con il punto!! Ora lo soooo :D
 console.log(ticket_cost);
 
 
@@ -34,16 +35,17 @@ console.log(ticket_cost);
 const discount_20 = (ticket_cost * 20) / 100;
 console.log("sconto 20% " + discount_20);
     // calcolo il 40%
-const discount_40 = (ticket_cost *40) / 100;
+const discount_40 = (ticket_cost * 40) / 100;
 console.log("sconto 40% " + discount_40);
  
 
 // APPLICO SCONTI e STAMPO in PAGINA :)
-if (passenger_age <= 18) 
+if (passenger_age < 18) 
 {
     let discount_under18 = (ticket_cost - discount_20).toFixed(2);
     console.log("siccome sei minorenne, paghi: " + discount_under18 + " €");
     document.getElementById("price").innerHTML = discount_under18;
+    // si può scrivere anche "questa cosa è uguale a se stessa - un valore, posso scrivere: ticket_cost -= discount_20", idem per il +=
 }
 else if (passenger_age >= 65)
 {
@@ -53,7 +55,15 @@ else if (passenger_age >= 65)
 }
 else {
     console.log("non hai diritto a nessuno sconto, paghi: " + ticket_cost.toFixed(2) + " €");
-    document.getElementById("price").innerHTML = ticket_cost;
+    document.getElementById("price").innerHTML = ticket_cost.toFixed(2);
 }
-//.toFixed(2) --> arrotondo a solo due decimali!
+//.toFixed(2) --> arrotondo a solo due decimali! SICCOME trasforma il numero in stringa, va aggiunto solo alla fine, quando non vanno fatti altri conti con quel numero!
 
+
+// e se qualcuno inserisce una lettera anzichè un numero? no problem! 
+if ((isNaN(passenger_age)) || (isNaN(travel_km))) {
+    console.log("Uno o più valori inseriti non sono validi, ricarica la pagina per riprovare.");
+} // a far bene andrebbe messo all'inizio dell'if qu sopra: se è così stampa messaggio di errore, se invece non è così allora esegui questo codice
+
+
+// NB: dando a tutti lo stesso nome, avrei evitato di dover stampare tre volte il document.get ecc -- come? invece che let blabla hanno scritto solo blabla :) Da ricontrollare!
